c7f8136e9b4a08020f720f42afc0a188b858853a
Валентин <astrotrain55@gmail.com>
2023-12-31 20:38:41 +0500

    Updates

--- assets/ProjectView-Hmc6J3rI.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,c as t,t as o,o as c}from"./index-kwZHnAB0.js";const a={};function n(e,s){return c(),t("div",null,o(e.$route.params.id),1)}const _=r(a,[["render",n]]);export{_ as default};
